c499e3930f89c6559a55a84c9a5462535325c171
tkuti <tunde.kuti@gmail.com>
2022-01-04 20:08:56 +0100

    checking winner, endgame

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,39 +1,27 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Square from './Square'
 
-const Board = () => {
-  const [xIsNext, setXIsNext] = useState(true)
-  const [squares, setSquares] = useState(Array(9).fill(null))
-
-  const updateSquares = (i) => {
-    const updatedSquares = squares.slice()
-    updatedSquares[i] = xIsNext ? 'X' : 'O'
-    setSquares(updatedSquares)
-    setXIsNext(!xIsNext)
-  }
-
-  const renderSquare = i => {
-    return <Square value={squares[i]} updateSquare={() => updateSquares(i)} />
+const Board = ({squares, updateSquares}) => {
+  
+  const renderSquare = (x, y) => {
+    return (
+      <Square
+        key={x + y}
+        value={squares[x][y]}
+        updateSquare={() => updateSquares(x, y)}
+      />
+    )
   }
 
   return (
     <div>
-      <div className='status'>Next player: {xIsNext ? 'X' : 'O'}</div>
-      <div className='board-row'>
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className='board-row'>
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className='board-row'>
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
+      {squares.map((row, indexRow) => (
+        <div className='board-row' key={indexRow}>
+          {row.map((cell, indexColumn) =>
+            renderSquare(indexRow, indexColumn)
+          )}
+        </div>
+      ))}
     </div>
   )
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,15 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Board from './Board'
 
 const Game = () => {
+  const size = 9
+  const [xIsNext, setXIsNext] = useState(true)
+  const [squares, setSquares] = useState(
+    Array.from(Array(size), () => new Array(size).fill(null))
+  )
+  const [winner, setWinner] = useState(false)
+
+  const updateSquares = (x, y) => {
+    if (winner || squares[x][y]) return
+    const updatedSquares = squares.slice()
+    updatedSquares[x][y] = xIsNext ? 'X' : 'O'
+    setSquares(updatedSquares)
+    setXIsNext(!xIsNext)
+    const hasWinner = calculateWinner(x, y)
+    if (hasWinner) {
+      setWinner(hasWinner)
+    }
+  }
+
+  const calculateWinner = (x, y) => {
+    const row = squares[x]
+    const isWinningRow = isWinningLine(row)
+    if (isWinningRow) return isWinningRow
+
+    const column = squares.map(row => row[y])
+    const isWinningColumn = isWinningLine(column)
+    if (isWinningColumn) return isWinningColumn
+
+    let diagonal = []
+    for (let i = 0; i < squares.length; i++) {
+      let diff = i - (x - y)
+      if (diff >= 0 && diff < squares.length) {
+        diagonal.push(squares[i][diff])
+      }
+    }
+    const isWinningDiagonal = isWinningLine(diagonal)
+    if (isWinningDiagonal) return isWinningDiagonal
+
+    let antiDiagonal = []
+    for (let i = 0; i < squares.length; i++) {
+      let diff = (x + y) - i
+      if (diff >= 0 && diff < squares.length) {
+        antiDiagonal.push(squares[i][diff])
+      }
+    }
+    const isWinningAntiDiagonal = isWinningLine(antiDiagonal)
+    if (isWinningAntiDiagonal) return isWinningAntiDiagonal
+
+    return false
+  }
+
+  const isWinningLine = line => {
+    let counter = 0
+    for (let cell of line) {
+      if (cell === 'X') {
+        if (counter >= 0) {
+          counter++
+        } else {
+          counter = 0
+        }
+      } else if (cell === 'O') {
+        if (counter <= 0) {
+          counter--
+        } else {
+          counter = 0
+        }
+      } else {
+        counter = 0
+      }
+      const hasWinner = counter === 3 ? 'X' : counter === -3 ? 'O' : false
+      if (hasWinner) {
+        return hasWinner
+      }
+    }
+    return false
+  }
+
   return (
     <div className='game'>
       <div className='game-board'>
-        <Board />
+        <Board squares={squares} updateSquares={updateSquares}/>
       </div>
       <div className='game-info'>
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
+        {!winner ? (
+          <div className='status'>Next player: {xIsNext ? 'X' : 'O'}</div>
+        ) : (
+          <div className='winner'>Winner: {winner}</div>
+        )}
       </div>
     </div>
   )
